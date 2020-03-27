03d3cf3c7bdb316ba9fe7767c7736e35c3bd1d39
illulia <choidaehoon0507@gmail.com>
2020-03-27 20:45:56 +0900

    Vending Machine system final 20200327

--- Python While.py
+++ Python While.py
@@ -1,44 +1,48 @@
-cola = 5 #가격은 600원
-pepsi = 5 #가격은 500원
-milk = 5 # 가격은 400원
+cola = 1 #가격은 600원
+pepsi = 1 #가격은 500원
+milk = 1 # 가격은 400원
 while True:
     money = int(input("돈을 넣어 주세요."))
+    print('투입금액 : ' + str(money) + '원')
+    colaM = money - 600
+    pepsiM = money - 500
+    milkM = money - 400
     if money >= 600:
-        print("콜라 1번, 펩시 2번,우유 3번")
+        print("콜라 1번, 펩시 2번, 우유 3번")
         select = int(input("음료를 골라주세요."))
         if select == 1:
-            cola = cola -1
             if cola != 0:
-                print("콜라를 줍니다. 거스름돈은 %d원입니다." % (money - 600))
+                cola = cola -1
+                print("콜라를 줍니다. 거스름돈은 %d원입니다." % colaM)
             else:
                 print("콜라가 부족합니다. 다른 음료를 골라주세요. 돈을 반환 합니다.")
         elif select == 2:
-            pepsi = pepsi -1
             if pepsi != 0:
-                print("펩시를 줍니다. 거스름돈은 %d원입니다." % (money - 500))
+                pepsi = pepsi -1
+                print("펩시를 줍니다. 거스름돈은 %d원입니다." % pepsiM)
             else:
                 print("펩시가 부족합니다. 다른 음료를 골라주세요. 돈을 반환 합니다.")
         elif select == 3:
-            milk = milk -1
             if milk != 0:
-                print("우유를 줍니다. 거스름돈은 %d원입니다." % (money - 400))
+                milk = milk -1
+                print("우유를 줍니다. 거스름돈은 %d원입니다." % milkM)
             else:
                 print("우유가 부족합니다. 다른 음료를 골라주세요. 돈을 반환 합니다.")
         else:
             print("오류입니다. 돈을 반환합니다. 다시 넣어주세요")
     elif money >= 500:
-        print("펩시 1번,우유 2번")
+        print("펩시 1번, 우유 2번")
         select = int(input("음료를 골라주세요."))
         if select == 1:
-            pepsi = pepsi -1
             if pepsi != 0:
-                print("펩시를 줍니다. 거스름돈은 %d원입니다." % (money - 500))
+                pepsi = pepsi -1
+                print("펩시를 줍니다. 거스름돈은 %d원입니다." % pepsiM)
             else:
                 print("펩시가 부족합니다. 다른 음료를 골라주세요. 돈을 반환 합니다.")    
         elif select == 2:
-            milk = milk -1
             if milk != 0:
-                print("우유를 줍니다. 거스름돈은 %d원입니다." % (money - 400))
+                milk = milk -1
+                print("우유를 줍니다. 거스름돈은 %d원입니다." % milkM)
             else:
                 print("우유가 부족합니다. 다른 음료를 골라주세요. 돈을 반환 합니다.")
         else:
@@ -47,18 +51,18 @@ while True:
         print("우유 1번")
         select = int(input("음료를 골라주세요."))
         if select == 1:
-            milk = milk -1
             if milk != 0:
-                print("우유를 줍니다. 거스름돈은 %d원입니다." % (money - 400))
+                milk = milk -1
+                print("우유를 줍니다. 거스름돈은 %d원입니다." % milkM)
             else:
                 print("우유가 부족합니다. 다른 음료를 골라주세요. 돈을 반환 합니다.")
         else:
             print("오류입니다. 돈을 반환합니다. 다시 넣어주세요")
     elif money < 400:
         print("돈이 부족합니다. 최소한 %d원은 더 넣어주세요." % (400 - money))
-    elif pepsi == cola == milk == 0:
+    if pepsi == cola == milk == 0:
         print("음료가 없습니다. 자판기를 종료합니다")
-        brek
+        break
 
        
             
