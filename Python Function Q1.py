20ad932eb60b8c0b6c9dc871d5518fdd10e80d0e
illulia <choidaehoon0507@gmail.com>
2020-03-30 20:57:44 +0900

    Python Quiz

--- Python Function Q1.py
+++ Python Function Q1.py
@@ -1,4 +1,4 @@
-def is_odd(b):
+def is_odd(b): # 입력 받은 숫자의 홀짝을 가려내는 함수
     if b%2 == 1:
         print("a는 홀수 입니다.")
     else:
--- Python Function Q2.py
+++ Python Function Q2.py
@@ -1,6 +1,6 @@
-def add(*number):
+def add(*number): #입력받은 숫자를 모두 더해서 평균을 내는 함수
     a = 0
     for c in number:
         a += c
     print("%d" %(a/len(number)))
-a =add(20,40,50,70)
+add(20,40,50,70)
