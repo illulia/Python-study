8a7c53fd2c4523788e2ad37dcbec2922eb7919b0
illulia <choidaehoon0507@gmail.com>
2020-03-30 10:36:52 +0900

    Python Question

--- Python For Q4.py
+++ Python For Q4.py
@@ -1,2 +1,2 @@
-for  a in range(1,101):
+for  a in range(1,101): #1부터 100까지 출력
     print(a)
--- Python For Q6.py
+++ Python For Q6.py
@@ -1,3 +1,3 @@
-numbers = [1, 2, 3, 4, 5]
+numbers = [1, 2, 3, 4, 5] #리스트 내포를 이용해 홀수만 출력하기
 result = [i*2  for i in numbers if i % 2 == 1]
 print(result)
