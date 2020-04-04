9f512780476b8406f734fd31a0eed62f3eb7c6ed
illulia <choidaehoon0507@gmail.com>
2020-04-04 18:59:39 +0900

    Vending Machine System 4.0

--- Vending3.py
+++ Vending3.py
@@ -5,15 +5,16 @@ while True:
     print('투입금액 : %d원' %money)
     if money >= 400 :
         print("콜라 1번, 펩시 2번, 우유 3번")
-        select = int(input("음료를 골라주세요."))
+        select = int(input("숫자를 입력하세요."))
         for i in range(1,4):
             if i == select:
                 Vending.Classifi(money,select)
-        if select > 3:
+        if select > 3 and select != 4:
             print(Vending.Error)
+        elif select 4:
+            print("관리자 모드를 실행합니다.")
     else: #돈을 400원 보다 적게 넣었을떄
         print("돈이 부족합니다. 최소한 %d원은 더 넣어주세요." % (400 - money))
     if Vending.a[1][1][0] < 1 and Vending.a[2][1][0] < 1 and Vending.a[3][1][0] < 1: #음료가 없을 경우
-        
         print("음료가 없습니다. 자판기를 종료합니다.") 
         break
